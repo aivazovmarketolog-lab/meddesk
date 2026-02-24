7e0098a39b2227f02a039ce6aac443bdcd444d0b
Нурадин <iii@MacBook-Pro-iii.local>
2026-02-25 00:10:31 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // Чтобы сборка корректно работала на GitHub Pages (в подпапке репозитория)
-  base: "/meddesk/",
+  base: "./",
   server: {
     host: "::",
     port: 8080,
